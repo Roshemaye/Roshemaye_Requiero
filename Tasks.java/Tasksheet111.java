7b5bee784a91ff81f9a8fd339435bc1b387aeb0a
Roshemaye <roshemayerequiero.edu@gmail.com>
2024-10-14 09:32:06 +0000

    done

---
+++ Tasks.java/Tasksheet111.java
@@ -0,0 +1,10 @@
+public class Tasksheet111 {
+    /** 
+    
+     * simply prints "KamustaMundo!" to standard output.
+    */
+        public static void main(String[] args) {
+            System.out.println("KamustaMundo!"); 
+            // Display the string.
+        }
+    }
